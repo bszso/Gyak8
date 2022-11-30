235e6fc49ee07a3c90eb57566c51b3b00fcc71b7
bszso <117721692+bszso@users.noreply.github.com>
2022-11-30 11:58:11 +0100

    gyak10

---
+++ Gyak8/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Gyak8.Models;
+
+namespace Gyak8.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+            return new JsonResult(kérdések);
+        }
+    }
+}
--- Gyak8/Program.cs
+++ Gyak8/Program.cs
@@ -19,8 +19,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseDefaultFiles();
-app.MapControllers();
+
 app.UseStaticFiles();
+app.MapControllers();
 //app.UseAuthorization();
 
 app.Run();
